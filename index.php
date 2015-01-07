ae4667aab86af4bbd4862b4abf93c5858bcc2fe3
Edwin Pham <edwin.pham@yahoo.com>
2015-01-07 11:10:30 -0800

    Initial setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Lab 01</title>
+    </head>
+    <body>
+        <?php
+        echo "Hello out there... I live";
+        ?>
+    </body>
+</html>
+
+
